b4e4baf86131569aad95315281f4f62b1e96302b
rony <rniumo@gmail.com>
2023-06-21 09:16:33 +0600

    init

---
+++ heap.cpp
@@ -0,0 +1,74 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int leftIdx(int i) {
+    return 2*i;
+}
+
+int rightIdx(int i) {
+    return 2*i + 1;
+}
+// MaxHeap
+void maxHeapify(int heap[], int heapsize, int i) {
+    int l, r;
+    int largest = i; // assumption
+    l = leftIdx(i);
+    r = rightIdx(i);
+
+    if (l <= heapsize && heap[l] > heap[i]) {
+        largest = l;
+    }
+    if (r <= heapsize && heap[r] > heap[largest]) {
+        largest = r;
+    }
+
+    if (i != largest) {
+        int temp = heap[i];
+        heap[i] = heap[largest];
+        heap[largest] = temp;
+        maxHeapify(heap, heapsize, largest);
+    }
+}
+
+void buildMaxHeap(int arr[], int heapsize) {
+    for (int i = heapsize/2; i >= 1; --i) {
+        maxHeapify(arr, heapsize, i);
+    }
+}
+
+// Minheap
+void minHeapify(int heap[], int heapsize, int i) {
+    int l, r;
+    l = leftIdx(i);
+    r = rightIdx(i);
+    int lowest = i;
+    if (l <= heapsize && heap[l] < heap[lowest]) {
+        lowest = l;
+    }
+    if (r <= heapsize && heap[r] < heap[lowest]) {
+        lowest = r;
+    }
+    if (i != lowest) {
+        int temp = heap[i];
+        heap[i] = heap[lowest];
+        heap[lowest] = temp;
+        minHeapify(heap, heapsize, lowest);
+    }
+}
+
+void buildMinHeap(int heap[], int heapsize) {
+    for (int i = heapsize/2; i >= 1; --i) {
+        minHeapify(heap, heapsize, i);
+    }
+}
+
+int main() {
+    const int heapsize = 5;
+    int heap[] = {0, 7, 1, 2,-3,5}; // zero index should be null
+    buildMinHeap(heap, heapsize);
+    // minHeapify(heap, 5, 2);
+    for (int i = 0; i <= heapsize; i++) {
+        cout << heap[i] << " ";
+    }
+}
