f5947ff4c6e9533d58afc86a427737547ad08f27
rony <rniumo@gmail.com>
2023-08-25 10:41:27 +0600

    fixes

--- Graph/bicoloring.cpp
+++ Graph/bicoloring.cpp
@@ -9,25 +9,25 @@ class Bicolor {
     private:
         AdjacencyList adj_list;
         vector<Colors> colors;
-        vector<int> explored_nodes;
-        static constexpr int UNEXPLORED = 0;
-        static constexpr int EXPLORED = 1;
+        vector<int> levels;
+        static constexpr int UNKNOWN = -1;
     public:
         Bicolor(AdjacencyList& _adj_list)
             : adj_list{_adj_list}
             , colors{vector<Colors>(_adj_list.size(), Colors::WHITE)}
-            , explored_nodes{vector<int>(_adj_list.size(), UNEXPLORED)}
+            , levels{vector<int>(_adj_list.size(), UNKNOWN)}
         {}
         bool is_bicolorable(int start) {
             queue<int> q;
             colors[start] = Colors::RED;
+            levels[start] = 0;
             q.push(start);
             while (!q.empty()) {
                 int u = q.front();
                 q.pop();
-                explored_nodes[u] = EXPLORED;
                 for (int v: adj_list[u]) {
-                    if (explored_nodes[v] == UNEXPLORED) {
+                    if (levels[v] == UNKNOWN) {
+                        levels[v] = levels[u] + 1;
                         q.push(v);
                     }
                     if (colors[v] == Colors::WHITE) {
@@ -48,8 +48,8 @@ class Bicolor {
 };
 
 int main() {
-    AdjacencyList a_list(4);
-    vector<pair<int, int>> edges{{0,2}, {2,3}, {3,1}, {1,0}};
+    AdjacencyList a_list(3);
+    vector<pair<int, int>> edges{{0,2}, {2,1}, {1,0}};
     for (auto [u,v]: edges) {
         a_list[u].push_back(v);
     }
